6f633233c5d22c82dd70d3617c2187cb30de7171
Albin <Albin@DESKTOP-2LCI1U3>
2023-06-28 09:30:56 +0200

    Projektdateien hinzufügen.

---
+++ Minidaten F#/Program.fs
@@ -0,0 +1,26 @@
+﻿printfn "Vorname:"
+let Vorname = System.Console.ReadLine();
+
+printfn "Nachname:"
+let Nachname = System.Console.ReadLine();
+
+printfn "Straße:"
+let Straße = System.Console.ReadLine();
+
+printfn "Hausnummer:"
+let Hausnummer = System.Console.ReadLine();
+
+printfn "Telefonnummer:"
+let Telefonnummer = System.Console.ReadLine();
+
+printfn "PLZ:"
+let PLZ = System.Console.ReadLine();
+
+printfn "Stadt:"
+let Stadt = System.Console.ReadLine();
+
+printfn "Geburtsdatum:"
+let Geburtsdatum = System.Console.ReadLine();
+
+printfn "Einkommen:"
+let Einkommen = System.Console.ReadLine();
