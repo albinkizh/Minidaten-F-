78fc77db7b9f52250a97e20fafc65f53ca83d822
Albin <Albin@DESKTOP-2LCI1U3>
2023-06-28 13:43:03 +0200

    putting in the oven 🔥

--- Minidaten F#/Program.fs
+++ Minidaten F#/Program.fs
@@ -1,72 +1,130 @@
-﻿let loop = 2
+﻿let mutable Break = false
+let mutable Break2 = false
+let mutable Vorname = ""
+let mutable Nachname = ""
+let mutable Straße = ""
+let mutable Hausnummer = ""
+let mutable Telefonnummer = ""
+let mutable PLZ = ""
+let mutable Stadt = ""
+let mutable Geburtsdatum = ""
+let mutable Einkommen = ""
 
-while (1 < loop) do
+let loop = 1
+let saves = [""]
+let updatedsave = [""]
+
+while not Break do
 
 printfn "Vorname:"
-let Vorname = System.Console.ReadLine();
+let Vornameant = System.Console.ReadLine();
+Vorname <- Vorname + Vornameant
 
-printfn "Nachname:"
-let Nachname = System.Console.ReadLine();
+printfn "
+Nachname:"
+let Nachnameant = System.Console.ReadLine();
+Nachname <- Nachname + Nachnameant
 
-printfn "Straße:"
-let Straße = System.Console.ReadLine();
+printfn "
+Straße:"
+let Straßeant = System.Console.ReadLine();
+Straße <- Straße + Straßeant
 
-printfn "Hausnummer:"
-let Hausnummer = System.Console.ReadLine();
+printfn "
+Hausnummer:"
+let Hausnummerant = System.Console.ReadLine();
+Hausnummer <- Hausnummer + Hausnummerant
 
-printfn "Telefonnummer:"
-let Telefonnummer = System.Console.ReadLine();
+printfn "
+Telefonnummer:"
+let Telefonnummerant = System.Console.ReadLine();
+Telefonnummer <- Telefonnummer + Telefonnummerant
 
-printfn "PLZ:"
-let PLZ = System.Console.ReadLine();
+printfn "
+PLZ:"
+let PLZant = System.Console.ReadLine();
+PLZ <- PLZ + PLZant
 
-printfn "Stadt:"
-let Stadt = System.Console.ReadLine();
+printfn "
+Stadt:"
+let Stadtant = System.Console.ReadLine();
+Stadt <- Stadt + Stadtant
 
-printfn "Geburtsdatum:"
-let Geburtsdatum = System.Console.ReadLine();
+printfn "
+Geburtsdatum:"
+let Geburtsdatumant = System.Console.ReadLine();
+Geburtsdatum <- Geburtsdatum + Geburtsdatumant
 
-printfn "Einkommen:"
-let Einkommen = System.Console.ReadLine();
+printfn "
+Einkommen:"
+let Einkommenant = System.Console.ReadLine();
+Einkommen <- Einkommen + Einkommenant
+System.Console.Clear();
 
-let Name = printfn $"
-{Vorname} {Nachname}"
-let Addresse = printfn $"{Straße} {Hausnummer}, {PLZ} {Stadt}"
-let Geburtstag = printfn $"{Geburtsdatum}"
-let Lohn = printfn $"Einkommen: {Einkommen} Euro"
+printfn $"
+{Vorname} {Nachname}, {Telefonnummer}"
+printfn $"{Straße} {Hausnummer}, {PLZ} {Stadt}"
+printfn $"{Geburtsdatum}"
+printfn $"Einkommen: {Einkommen} Euro"
 
-printfn "Stimmt alles?"
+printfn "
+Stimmt alles?"
 let Frage = System.Console.ReadLine();
 
 if Frage = "Ja" then
+
     System.Console.Clear();
+    Break <- true
     printfn "Daten gespeichert!
-    "
-    
+    " 
+    let updatedsave = [$"{Vorname} "; $"{Nachname} "; $"{Telefonnummer} "; $"{Straße} "; $"{Hausnummer} "; $"{PLZ} "; $"{Stadt} "; $"{Geburtsdatum} "; $"{Einkommen} "]
+    let saves = updatedsave @ []
+    List.iter (fun item -> printf "%s" item) saves
 
 else 
     System.Console.Clear();
     printfn "Dann ein nochmal!
-    " 
+    "
+    Vorname <- ""
+    Nachname <- ""
+    Telefonnummer <- ""
+    Straße <- ""
+    Hausnummer <- ""
+    PLZ <- ""
+    Stadt <- ""
+    Geburtsdatum <- ""
+    Einkommen <- ""
+
+
+done
+
+while not Break2 do 
+
+printfn "Was möchten Sie filtern? (Adresse, Kontaktinformationen, Einkommen)"
+let Auswahl = System.Console.ReadLine(); 
+
+if Auswahl = "Adresse" then printfn $"
+{Straße} {Hausnummer}, {PLZ} {Stadt}"
+if Auswahl = "Kontaktinformationen" then printfn $"
+{Vorname} {Nachname},
+{Telefonnummer} 
+{Geburtsdatum}"
+if Auswahl = "Einkommen" then printfn $"
+Ihr Einkommen ist: {Einkommen}"
 
 
 
 
+printfn "
+Nochmal? (Ja/Nein)"
+let Break2answer = System.Console.ReadLine();
+if Break2answer = "Ja" then 
+    System.Console.Clear(); 
+    printfn "Okay!"  
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-loop = loop + 1
-
-
+if Break2answer = "Nein" then  
+    System.Console.Clear(); 
+    printfn "Aufwiedersehen!" 
+    Break2 <- true
+done
